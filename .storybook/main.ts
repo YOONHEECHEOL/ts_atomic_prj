bf7ab5995f503f7804bdadb758b7801ffabbe9ed
hcyoon0511 <hcyoon0511@douzone.com>
2023-08-17 14:49:14 +0900

    init

---
+++ .storybook/main.ts
@@ -0,0 +1,45 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+import path from "path";
+const toPath = (_path) => path.join(process.cwd(), _path);
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+  webpackFinal(config, options) {
+    config.module?.rules?.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          ['react-app', { flow: false, typescript: true }],
+          require.resolve('@emotion/babel-preset-css-prop'),
+        ]
+      }
+    });
+    config.resolve?.extensions?.push('.ts', '.tsx');
+    if(config.resolve)
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        '@emotion/core': toPath('node_modules/@emotion/react'),
+        '@emotion/styled': toPath('node_modules/@emotion/styled'),
+        'emotion-theming': toPath('node_modules/@emotion/react'),
+      }
+
+    return config;
+  },
+};
+export default config;
