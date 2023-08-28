31bf69b315e7510e3d606da2bdfdecd5c1ba87cc
YOONHEECHEOL <luk2903201@gmail.com>
2023-08-28 23:41:55 +0900

    💄 Update styled component ex) InputTest.tsx

--- src/component/atom/Input/Input.tsx
+++ src/component/atom/Input/Input.tsx
@@ -1,21 +1,20 @@
-import { css, keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
-import { useRef, useState } from 'react';
+import { css, keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { forwardRef, useRef, useState } from "react";
 
 interface InputProps {
     value?: string;
     placeholder?: string;
     border?: true | false;
-    size?: 's' | 'm' | 'l';
+    size?: "s" | "m" | "l";
 }
 
 export default function Input({
-    value = '',
-    placeholder = '입력하세요',
+    value = "",
+    placeholder = "입력하세요",
     border = true,
-    size = 'm',
+    size = "m",
 }: InputProps) {
-
     const refObj = useRef<HTMLInputElement | any>(null);
 
     let [inputValue, setInputValue] = useState(value);
@@ -28,98 +27,118 @@ export default function Input({
         50% {
             border-color: #222
         }
-    `
+    `;
 
-    const InputWrap = styled.div({
-        position: 'relative',
-        display: 'flex',
-        justifyContent: 'left',
-        fontSize: `${size === 's' ?
-            '1.6rem' : size === 'm' ?
-                '2rem' : size === 'l' ?
-                    '2.4rem' : '2rem'}`,
-        overflow: 'hidden',
+    const InputWrap = styled.div<{ size: string; onClick: any }>(
+        {
+            position: "relative",
+            display: "flex",
+            justifyContent: "left",
+            overflow: "hidden",
+            width: "100%",
+            minWidth: "100%",
+            padding: "0 0 .2rem 0",
+            borderBottom: "1px solid #222",
+        },
+        (props: any) => ({
+            fontSize: `${
+                props.size === "s"
+                    ? "1.6rem"
+                    : props.size === "m"
+                    ? "2rem"
+                    : props.size === "l"
+                    ? "2.4rem"
+                    : "2rem"
+            }`,
+            minHeight: `${
+                props.size === "s"
+                    ? "2rem"
+                    : props.size === "m"
+                    ? "2.4rem"
+                    : props.size === "l"
+                    ? "2.8rem"
+                    : "2.4rem"
+            }`,
+        })
+    );
+    const RenderInputWrap = forwardRef((props: any, ref: any) => {
+        return (
+            <InputWrap size={"m"} onClick={(e: any) => ref?.current?.focus()}>
+                {props?.children}
+            </InputWrap>
+        );
+    });
 
-        width: '100%',
-        minWidth: '100%',
-        minHeight: `${size === 's' ?
-            '2rem' : size === 'm' ?
-                '2.4rem' : size === 'l' ?
-                    '2.8rem' : '2.4rem'}`,
-        padding: '0 0 .2rem 0',
+    const InputArea = styled.input<{ ref: any }>({
+        position: "absolute",
+        top: "-4px",
+        left: "0",
 
-        borderBottom: '1px solid #222',
-    })
+        display: "block",
+        marginTop: "-1rem",
 
-    const InputArea = styled.input({
-        position: 'absolute',
-        top: '-4px',
-        left: '0',
-
-        display: 'block',
-        marginTop: '-1rem',
-
-        borderWidth: border ? '1px' : '0px',
-        borderStyle: border ? 'solid' : '',
-        borderColor: border ? '#222' : '#fff',
-        borderRadius: border ? '0.4rem' : '0',
-        borderBottom: '1px solid #222',
+        borderWidth: border ? "1px" : "0px",
+        borderStyle: border ? "solid" : "",
+        borderColor: border ? "#222" : "#fff",
+        borderRadius: border ? "0.4rem" : "0",
+        borderBottom: "1px solid #222",
 
         // cursor 스타일
-        caretColor: 'transparent',
+        caretColor: "transparent",
 
         // input 기본 스타일 초기화
-        WebkitAppearance: 'none',
-        MozAppearance: 'none',
-        appearance: 'none',
+        WebkitAppearance: "none",
+        MozAppearance: "none",
+        appearance: "none",
 
         // focus 효과 초기화
-        '&:focus': {
-            outline: 'none',
+        "&:focus": {
+            outline: "none",
         },
 
         // placeholder 스타일
-        '&::placeholder': {
-            color: '#ddd'
-        }
-    })
+        "&::placeholder": {
+            color: "#ddd",
+        },
+    });
+    const RenderInputArea = forwardRef((props: any, ref: any) => {
+        return <InputArea ref={ref} />;
+    });
 
     const InputText = styled.span<{ t: string }>`
         display: block;
         color: #222;
-    `
+    `;
 
     const Cursor = styled.span({
-        display: 'block',
-        borderLeft: '.4rem solid #222',
+        display: "block",
+        borderLeft: ".4rem solid #222",
         animation: `${blink} 1s ease infinite`,
-    })
+    });
 
     return (
-        <InputWrap
+        <RenderInputWrap
             onClick={() => {
-                console.log(refObj)
-                refObj?.current?.focus();
+                console.log(refObj);
             }}
+            size={"m"}
         >
-            <InputArea
+            <RenderInputArea
                 ref={refObj}
                 type="text"
                 value={inputValue}
-                onChange={(e) => {
+                onChange={(e: any) => {
                     setInputValue(e.target.value);
                     console.log(e.target.value);
                 }}
                 placeholder={placeholder}
             />
-            {
-                inputValue.length === 0 ?
-                    <span
-                    >{placeholder}</span>
-                    : <span
-                    >{inputValue}</span>
-            }
+            {inputValue.length === 0 ? (
+                <span>{placeholder}</span>
+            ) : (
+                <span>{inputValue}</span>
+            )}
             <Cursor></Cursor>
-        </InputWrap>
-    )
+        </RenderInputWrap>
+    );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,56 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Global } from '@emotion/react';
-import { reset } from './style/reset';
-import { createBrowserRouter, Link, Navigate, redirect, Route, RouterProvider, Routes } from 'react-router-dom';
-import ErrorPage from './component/page/ErrorPage/ErrorPage';
-import { getCookie } from './utils/cookieUtils';
-import LoginTemplate from './component/template/LoginTemplate';
-import styled from '@emotion/styled';
-import Button from './component/atom/Button/Button';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Global } from "@emotion/react";
+import { reset } from "./style/reset";
+import {
+    createBrowserRouter,
+    Link,
+    Navigate,
+    redirect,
+    Route,
+    RouterProvider,
+    Routes,
+} from "react-router-dom";
+import ErrorPage from "./component/page/ErrorPage/ErrorPage";
+import { getCookie } from "./utils/cookieUtils";
+import LoginTemplate from "./component/template/LoginTemplate";
+import styled from "@emotion/styled";
+import Button from "./component/atom/Button/Button";
+import InputTest, { TestInput } from "./component/atom/Input/InputTest";
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 const ROOT_PATH = "gsp-front";
 
 const authCheck = (children: any) => {
-
-    const Test = styled.div({ display: 'flex', justifyContent: 'right', flexDirection: 'column', alignItems: 'center' })
+    const Test = styled.div({
+        display: "flex",
+        justifyContent: "right",
+        flexDirection: "column",
+        alignItems: "center",
+    });
 
     // 로그인 시 세션 체크 로직 생성
 
-    const isLogin = getCookie('isLogin');
-    if (isLogin !== 'Y')
-        return (
-            <Navigate to={'/login'} replace={false} />
-        )
+    const isLogin = getCookie("isLogin");
+    if (isLogin !== "Y") return <Navigate to={"/login"} replace={false} />;
     else
         return (
             <div>
                 <Test>
-                    <span style={{ width: '300px' }}>
-                        {children}
-                    </span>
+                    <span style={{ width: "300px" }}>{children}</span>
                 </Test>
             </div>
-        )
-}
+        );
+};
 
 const router = createBrowserRouter([
     {
@@ -51,7 +59,11 @@ const router = createBrowserRouter([
     },
     {
         path: ROOT_PATH + "/",
-        element: authCheck(<div>Hello! / <Button label='123' /> <Link to="1234">Check</Link></div>),
+        element: authCheck(
+            <div>
+                Hello! / <Button label="123" /> <Link to="1234">Check</Link>
+            </div>
+        ),
     },
     {
         path: ROOT_PATH + "/App",
@@ -59,7 +71,15 @@ const router = createBrowserRouter([
     },
     {
         path: "/login",
-        element: <LoginTemplate><div>123</div></LoginTemplate>
+        element: (
+            <LoginTemplate>
+                <div>123</div>
+            </LoginTemplate>
+        ),
+    },
+    {
+        path: "/refTest",
+        element: <TestInput />,
     },
 ]);
 
